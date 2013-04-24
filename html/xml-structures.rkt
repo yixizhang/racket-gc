bdfa123929aa8588281f9d089ad97ceb7f230dbe
Yixi Zhang <yixi.zhang.max@gmail.com>
2013-04-24 16:35:34 -0500

    combine xml-structures and xml-core
    To provide (all-defined-out),
    so all variables, structs and ad-hoc
    procedures are provided.

--- html/xml-structures.rkt
+++ html/xml-structures.rkt
@@ -2,22 +2,39 @@
 
 (allocator-setup "../incr-struct.rkt" 200)
 
-(require "xml-core.rkt")
+(provide (all-defined-out))
 
-(provide location
-         source
-         external-dtd
-         document-type
-         comment
-         p-i
-         prolog
-         document
-         element
-         attribute
-         permissive-xexprs
-         pcdata
-         cdata
-         entity)
+; permissive-xexprs : parameter bool
+;;(define permissive-xexprs (make-parameter #f))
+(define permissive-xexprs #f)
+
+; Source = (make-source Location Location)
+;;(define-struct source (start stop) #:transparent)
+(define-struct source (start stop))
+
+; Comment = (make-comment String)
+(define-struct comment (text))
+
+; Processing-instruction = (make-p-i Location Location String String)
+; also represents XMLDecl
+(define-struct (p-i source) (target-name instruction))
+
+; Pcdata = (make-pcdata Location Location String)
+(define-struct (pcdata source) (string))
+
+; Cdata = (make-cdata Location Location String)
+(define-struct (cdata source) (string))
+
+; Section 2.2 of XML 1.1
+; (XML 1.0 is slightly different and looks less restrictive)
+(define (valid-char? i)
+  (and (exact-nonnegative-integer? i)
+       (or (= i #x9)
+           (= i #xA)
+           (= i #xD)
+           (<= #x20 i #xD7FF)
+           (<= #xE000 i #xFFFD)
+           (<= #x10000 i #x10FFFF))))
 
 ; Location = (make-location Nat Nat Nat) | Symbol
 ;;(define-struct location (line char offset) #:transparent)
